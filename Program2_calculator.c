ce0c7351f97cbb43d2a2c002278720cbfc609804
VIVEKSUDAN <146426115+VIVEKSUDAN@users.noreply.github.com>
2023-10-04 20:18:13 +0530

    Add files via upload

---
+++ Program2_calculator.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main(){
+
+    int a, b , c;
+
+    printf("Enter the value off the two number:");
+
+    scanf("%d%d", &a,&b);
+
+    printf("sum is : %d\n", c = a+ b);
+
+    printf("sub is : %d\n", c = a-b);
+
+    printf("prod is :%d\n", c = a*b);
+
+    printf("div is :%d", c = a/b);
+
+    return 0;
+} 
